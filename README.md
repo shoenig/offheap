91f0b6b3d6f665543129ca4acc2319460cc272fe
Seth Hoenig <seth.a.hoenig@gmail.com>
2015-02-25 16:59:44 -0600

    readme example

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # offheap
-offheap stuff go go
+
+Allocate system memory as if it were a []byte. 
+
+Useful for allocating massive buffers off of Go's heap, which would otherwise consume a lot of resources.
+
+### Example
+
+    package main
+
+    import (
+    	"fmt"
+
+	    "github/shoenig/offheap"
+    )
+
+    func main() {
+    	var m offheap.Memory 
+	    var e error
+	
+	    // allocate 5 MB
+	    if m, e = offheap.New(5 * 1024 * 1024); e != nil {
+    		panic(e)
+	    } 
+	    fmt.Println("m[0]", m[0])
+
+	    m[0] = 42
+	    fmt.Println("m[0]", m[0])
+
+	    if e = m.Unmap(); e != nil {
+		    panic(e)
+	    }
+	    fmt.Println("finished")
+    }
+
+
+
--- offheap.go
+++ offheap.go
@@ -1,3 +1,8 @@
+// Copyright 2015 Seth Hoenig. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package offheap provides a way to grab and use a chunk of system memory.
 package offheap
 
 import (
@@ -9,6 +14,7 @@ import (
 // without dealing with large heap issues.
 type Memory []byte
 
+// New creates an offheap Memory slice of the specified number of bytes.
 func New(bytes int64) (Memory, error) {
 	mapped, e := gommap.MapRegion(
 		0,
@@ -24,6 +30,8 @@ func New(bytes int64) (Memory, error) {
 	return Memory(mapped), nil
 }
 
+// Unmap will delete the offheap Memory slice. Any usage of the Memory
+// afterwords will cause a panic.
 func (m Memory) Unmap() error {
 	return gommap.MMap(m).UnsafeUnmap()
 }
